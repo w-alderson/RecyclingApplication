bc577e0cc20de67baccf77e4576b2e413239f298
Will Alderson <wja25@bath.ac.uk>
2019-10-25 20:42:14 +0100

    Initial Commit

---
+++ Recycling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recycling
+//
+//  Created by William Alderson on 25/10/2019.
+//  Copyright © 2019 William Alderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
